f6210a6755eb9377e5f12eca8bc2ce63a45c338f
julianafalcaoo <154533645+julianafalcaoo@users.noreply.github.com>
2026-09-21 22:32:24 -0400

    att

--- prisma/migrations/20260922022202_init_leads_otp/migration.sql
+++ prisma/migrations/20260922022202_init_leads_otp/migration.sql
@@ -1,4 +1,3 @@
--- CreateTable
 CREATE TABLE "Lead" (
     "id" TEXT NOT NULL,
     "nome" TEXT NOT NULL,
@@ -11,7 +10,6 @@ CREATE TABLE "Lead" (
     CONSTRAINT "Lead_pkey" PRIMARY KEY ("id")
 );
 
--- CreateTable
 CREATE TABLE "VerificacaoOtp" (
     "id" TEXT NOT NULL,
     "leadId" TEXT NOT NULL,
@@ -24,11 +22,8 @@ CREATE TABLE "VerificacaoOtp" (
     CONSTRAINT "VerificacaoOtp_pkey" PRIMARY KEY ("id")
 );
 
--- CreateIndex
 CREATE UNIQUE INDEX "Lead_email_key" ON "Lead"("email");
 
--- CreateIndex
 CREATE INDEX "VerificacaoOtp_leadId_createdAt_idx" ON "VerificacaoOtp"("leadId", "createdAt");
 
--- AddForeignKey
 ALTER TABLE "VerificacaoOtp" ADD CONSTRAINT "VerificacaoOtp_leadId_fkey" FOREIGN KEY ("leadId") REFERENCES "Lead"("id") ON DELETE CASCADE ON UPDATE CASCADE;
